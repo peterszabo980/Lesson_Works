050f405737e924e1664b2505424603d6e435fc20
Peter Szabo <szwazzup@gmail.com>
2022-04-11 21:59:13 +0200

    Visszaszámlálás - képernyő törléssel

---
+++ src/hu/progmatic/LW_20220319/LW_20220319_Visszaszamlalas.java
@@ -0,0 +1,24 @@
+package hu.progmatic.LW_20220319;
+
+public class LW_20220319_Visszaszamlalas {
+    public static void main(String[] args) throws InterruptedException {
+        int[] numbers = countDown(10);
+        for (int i = numbers.length - 1; i >= 0 ; i--) {
+
+            System.out.print(i);
+            //printWithDelays(" ", MILLISECONDS, 1000);
+            Thread.sleep(700);
+            System.out.print("\b\b\b\b");
+
+        }
+    }
+
+    public static int[] countDown(int from) {
+        int[] numbers = new int[from + 1];
+        for (int i = from; i >= 0 ; i--) {
+            numbers[from - i] = i;
+        }
+        return numbers;
+    }
+
+}
--- src/hu/progmatic/LW_20220319/Ora20220319_TombOsszeg.java
+++ src/hu/progmatic/LW_20220319/Ora20220319_TombOsszeg.java
@@ -2,19 +2,24 @@ package hu.progmatic.LW_20220319;
 
 public class Ora20220319_TombOsszeg {
     public static void main(String[] args) {
-        int[] szamok ={1, 2, 3, 4, 5};
+        int[] szamok ={4, -2, 23, -10};
         int sum = 0;
-        for (int i = 1; i <= szamok.length ; i++) {
+
+        /*
+        // szamok[0] + szamok[1] + szamok[2] + szamok[3]
+        // 4 + -2 + 23 + -10
+        for (int i = 0; i < szamok.length ; i++) {
             sum += szamok[i];
         }
         System.out.println(sum);
+         */
         System.out.println(elsoN(szamok));
     }
 
     public static int elsoN(int[] darab) {
         int sum = 0;
         //írjuk ki az első dbarab szám összegét
-        for (int i = 1; i <= darab.length ; i++) {
+        for (int i = 0; i < darab.length ; i++) {
             sum += darab[i];
         } return sum;
     }
